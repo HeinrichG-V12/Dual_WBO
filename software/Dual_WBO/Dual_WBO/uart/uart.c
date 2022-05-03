863d6a22ab5bfff3427d59b3e8676834efc6f7ee
Heinrich Geiger <heinrich.geiger@suedleasing.com>
2022-05-03 17:35:43 +0200

    uart buffer basics

--- software/Dual_WBO/Dual_WBO/uart/uart.c
+++ software/Dual_WBO/Dual_WBO/uart/uart.c
@@ -7,6 +7,14 @@
 
 #include "uart.h"
 
+uint8_t UART_RX_BUFFER[UART_RX_BUFFER_SIZE];
+static volatile uint8_t UART_rxHead;
+static volatile uint8_t UART_rxTail;
+
+uint8_t UART_TX_BUFFER[UART_TX_BUFFER_SIZE];
+static volatile uint8_t UART_txHead;
+static volatile uint8_t UART_txTail;
+
 void uart_init (void)
 {
 	//init uart 1 with 115200, 8n1
@@ -14,7 +22,12 @@ void uart_init (void)
 	UBRR1 = UBRR_115k;
 	
 	UCSR1B |= (1 << RXCIE1)|(1 << RXEN1)|(1 << TXEN1);
-	UCSR1C |= (1 << UCSZ11)|(1 << UCSZ10);	
+	UCSR1C |= (1 << UCSZ11)|(1 << UCSZ10);
+	UART_rxHead = 0;
+	UART_rxTail = 0;
+	UART_txHead = 0;
+	UART_txTail = 0;
+	
 }
 
 void uart_sendC (uint8_t data)
@@ -36,5 +49,17 @@ void uart_sendString (uint8_t *data, uint8_t length)
 
 ISR(USART1_RX_vect)
 {
-	//tue was
+	uint8_t data, tmphead;
+	
+	data = UDR1; /* read the received data */
+	/* calculate buffer index */
+	tmphead = ( UART_rxHead + 1 ) & UART_RX_BUFFER_MASK;
+	UART_rxHead = tmphead; /* store new index */
+	if ( tmphead == UART_rxTail )
+	{
+		/* ERROR! Receive buffer overflow */
+	}
+
+	UART_RX_BUFFER[tmphead] = data; /* store received data in buffer */
 }
+
--- software/Dual_WBO/Dual_WBO/uart/uart.h
+++ software/Dual_WBO/Dual_WBO/uart/uart.h
@@ -16,6 +16,12 @@
 #define UBRR_115k	8
 #define UBRR_250k	3
 
+#define UART_RX_BUFFER_SIZE		128
+#define UART_RX_BUFFER_MASK		( UART_RX_BUFFER_SIZE - 1 )
+#define UART_TX_BUFFER_SIZE		128
+#define UART_TX_BUFFER_MASK		( UART_TX_BUFFER_SIZE - 1 )
+
+
 void uart_init (void);
 void uart_sendC (uint8_t data);
 void uart_sendString (uint8_t *data, uint8_t length);
